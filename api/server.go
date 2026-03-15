00eb5774a78d66cdfe32e0c0bc09307a004a5c37
psea524 <p479664290@gmail.com>
2026-03-15 20:24:32 +0800

    增加测试模拟

--- api/server.go
+++ api/server.go
@@ -2013,6 +2013,7 @@ func (s *Server) handleCreateExchange(c *gin.Context) {
 	validTypes := map[string]bool{
 		"binance": true, "bybit": true, "okx": true, "bitget": true,
 		"hyperliquid": true, "aster": true, "lighter": true, "gate": true, "kucoin": true,
+		"paper": true,
 	}
 	if !validTypes[req.ExchangeType] {
 		c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Invalid exchange type: %s", req.ExchangeType)})
